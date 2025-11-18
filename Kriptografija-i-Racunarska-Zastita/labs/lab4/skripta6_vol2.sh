a0b4e5c4b6abaf6ddc5f369dfaee4c69f5852909
Andrej Trožić <andrejtrozic95@gmail.com>
2025-11-18 12:06:06 +0100

    Scripts updated ..

--- Kriptografija-i-Racunarska-Zastita/labs/lab4/skripta6_vol2.sh
+++ Kriptografija-i-Racunarska-Zastita/labs/lab4/skripta6_vol2.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # 8. GENERISANJE NOVIH KLJUČEVA I CSR (Čuvamo u 07/)
+echo "Eighth Task .."
 cd 07
 openssl genrsa -out novi2048.key 2048
 openssl genrsa -out novi4096.key 4096
@@ -9,6 +10,7 @@ openssl req -new -key novi4096.key -out novi2.csr -config ../02/openssl.cnf
 cd ..
 
 # 9. POTPISIVANJE NOVIH CSR (CA radi iz 02/, output u 08/)
+echo "Ninth Task .."
 cd 02
 # NAPOMENA: Rucna modifikacija openssl.cnf pre ovoga je ključna (basicConstraints i keyUsage/extendedKeyUsage)
 openssl ca -in ../07/novi1.csr -config openssl.cnf -days 5 -out ../08/novi1.pem
@@ -16,7 +18,7 @@ openssl ca -in ../07/novi2.csr -config openssl.cnf -days 5 -out ../08/novi2.pem
 cd ..
 
 # 10. VERIFIKACIJA NOVIH SERTIFIKATA
-echo "I'm here ..'"
+echo "Tenth Task'"
 openssl verify -CAfile 02/rootca.pem -verbose 08/novi1.pem
 openssl verify -CAfile 02/rootca.pem -verbose 08/novi2.pem
 openssl verify -CAfile 02/rootca.pem -purpose sslserver -verbose 08/novi1.pem
--- Kriptografija-i-Racunarska-Zastita/labs/lab4/skripta6_vol3.sh
+++ Kriptografija-i-Racunarska-Zastita/labs/lab4/skripta6_vol3.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
 # 11
-openssl ca -revoke ../08/novi1.pem -config ../02/openssl.cnf -crl_reason keyCompromise
+echo "Eleventh Task .."
+cd 02
+openssl ca -revoke ../08/novi1.pem -config openssl.cnf -crl_reason keyCompromise
 openssl ca -gencrl -out ../02/crl/rootcrl.pem -config ../02/openssl.cnf
 openssl crl -in ../02/crl/rootcrl.pem -noout -text
+cd ..
 
 # 12
-openssl pkcs12 -export -inkey ../07/novi2048.key -in ../08/novi1.pem -certfile ../02/rootca.pem -out novi.p12
+echo "Final Task .."
+openssl pkcs12 -export -inkey 07/novi2048.key -in 08/novi1.pem -certfile 02/rootca.pem -out novi.p12
 
 # 13
-openssl verify -CAfile ThawteSGCCA mail.google.com
-openssl verify el.etfbl.net
+# openssl verify -CAfile ThawteSGCCA mail.google.com
+# openssl verify el.etfbl.net
 
 # 14
-openssl crl -in pca1.1.1.crl -noout -text -inform DER
+# openssl crl -in pca1.1.1.crl -noout -text -inform DER

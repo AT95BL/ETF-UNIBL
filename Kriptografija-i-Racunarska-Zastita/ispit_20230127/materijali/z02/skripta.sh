695f206b74a5edeaf34c47d0e23d5d7d756414f7
Andrej Trožić <andrejtrozic95@gmail.com>
2025-11-24 15:46:10 +0100

    salt calculation added
    salt=$(echo "$hash1" | cut -d '$' -f 3)

--- Kriptografija-i-Racunarska-Zastita/ispit_20230127/materijali/z02/skripta.sh
+++ Kriptografija-i-Racunarska-Zastita/ispit_20230127/materijali/z02/skripta.sh
@@ -3,14 +3,16 @@
 # Lista otisaka koje treba pronaci
 otisci=(otisak1.dec otisak2.dec otisak3.dec)
 
-for otisak_datoteka in "${otisci[@]}"; do
+for otisak_datoteka in "${otisci[@]}"; 
+do
     echo "Trazi se kljuc za: $otisak_datoteka"
     hash1=$(cat "$otisak_datoteka")
 
-    for lozinka_datoteka in lozinka*.txt; do
+    for lozinka_datoteka in lozinka*.txt; 
+    do
         t=$(cat "$lozinka_datoteka")
-        # Pretpostavimo da je 'wAGnh5WA' salt za SVE
-        hash2=$(openssl passwd -apr1 -salt wAGnh5WA "$t")
+        salt=$(echo "$hash1" | cut -d '$' -f 3)
+        hash2=$(openssl passwd -apr1 -salt "$salt" "$t")
 
         if [ "$hash1" == "$hash2" ]; then
             echo -e "Pronadjena lozinka za $otisak_datoteka: $lozinka_datoteka (Sadrzaj: $t)"
